e066087c65cf1948c5f61c5c5f2b2a3eb55c3664
Richard Orr <richard@ourresearch.org>
2021-11-22 10:45:37 -0500

    remove null byte filter on PaperAbstractsInvertedIndex

--- sql/tables/PaperAbstractsInvertedIndex.sql
+++ sql/tables/PaperAbstractsInvertedIndex.sql
@@ -8,5 +8,5 @@ CREATE UNLOGGED TABLE mag.PaperAbstractsInvertedIndex(
     IndexedAbstract JSONB
   );
 
-\COPY mag.PaperAbstractsInvertedIndex(PaperId, IndexedAbstract) FROM PROGRAM 'awk FNR-1 input/nlp/PaperAbstractsInvertedIndex.txt* | sed "s/\\\\\\\\u0000//g"' null as '';
+\COPY mag.PaperAbstractsInvertedIndex(PaperId, IndexedAbstract) FROM PROGRAM 'awk FNR-1 input/nlp/PaperAbstractsInvertedIndex.txt*' null as ''i;
 
